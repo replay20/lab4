4e5a1aedad546fecab05cd1d81d9ffcf2126e572
replay20 <taglador@gmail.com>
2024-03-06 11:49:37 +0100

    zad1 gotowe

--- zad1.py
+++ zad1.py
@@ -1,9 +1,14 @@
 zdanie = input("Napisz dowolne zdanie: ")
 print(zdanie)
 print(type(zdanie))
+licznik = 0
 
 def Convert(string):
     lista = list (zdanie.split(" "))
     return lista
 
-print (Convert(zdanie))
+licz = Convert(zdanie)
+for x in licz:
+    licznik=licznik+1
+
+print(licznik)
