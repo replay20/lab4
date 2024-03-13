866384f38914b4de6b54f66c6c7c3272a01cc497
replay20 <taglador@gmail.com>
2024-03-13 11:28:41 +0100

    Zad 1 poprawione z zaokragleniem

--- zad4.py
+++ zad4.py
@@ -1,17 +1,49 @@
-liczba = input("Podaj liczbe: ")
-liczba = int(liczba)
+from decimal import *
 
-czyPierwsza = True
+# jakim typem jest range ?
+liczby = range(5)
+print(type(liczby)) # range jest obiektem typu range (w Python 2.x była to lista)
 
-if liczba <= 1 :
-    czyPierwsza = False
-elif liczba > 1 :
-    for i in range(2,liczba):
-        if (liczba % i) == 0:
-            czyPierwsza = False
-            break
+# range może przyjmować 1 parametr, wtedy jest to parametr stop
+for i in range(10):
+    print(i)
 
-if czyPierwsza == True:
-    print(liczba, "jest liczba pierwsza")
-else:
-    print(liczba, "nie jest liczba pierwsza")
+# range może też przyjmować 2 parametry (start, stop)
+for i in range(4, 10):
+    print(i)
+
+# lub 3 parametry (start, stop, step)
+for i in range(4, 10, 2):
+    print(i)
+
+# możemy również generować wartości ujemne
+for i in range(-5, -1):
+    print(i)
+
+for i in range(-5, -10, -2):
+    print(i)
+
+# lista z elementów funkcji range
+lista = list(range(10))
+print(lista)
+
+# funkcja range nie generuje wartości zmiennoprzecinkowych, ale można
+# dość łatwo taką funkcję stworzyć - poniżej funkcja generująca (yield)
+def frange(start, stop, step):
+    i = start
+    while i < stop:
+        yield i
+        i += step
+
+for i in frange(0.1, 0.5, 0.1):
+    print(i)
+
+# po wyświetleniu wyniku można się zdziwić bo 0.3 to właściwie nie 0.3...
+# to może jeszcze raz, ale tak
+for i in frange(0.1, 0.5, 0.1):
+    print(Decimal(i))
+
+print((0.1 + 0.2) == 0.3)  # kto by się spodziewał ?
+print(round((0.1 + 0.2), 2) == round(0.3, 2))  # teraz lepiej
+print((Decimal(0.1) + Decimal(0.2)) == Decimal(0.3))
+print((Decimal('0.1') + Decimal('0.2')) == Decimal('0.3'))
