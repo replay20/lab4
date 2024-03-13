b3adc2c9d2435f0a53c0e8f1ad726f21d2a8a31a
replay20 <taglador@gmail.com>
2024-03-13 11:18:18 +0100

    Zad 1 gotowe

---
+++ lab4/Zad1.py
@@ -0,0 +1,16 @@
+import math
+
+def dzialaniePierwsze(e):
+    return (e**4 - math.log(34,2))**(1/3)
+
+print(dzialaniePierwsze(2))
+
+def dzialanieDrugie(f):
+    return (math.log(20) + math.cos(45) + f)**(1/3)
+
+print(dzialanieDrugie(3))
+
+def dzialanieTrzecie():
+    return (math.log(32,2)+math.sin(45) * (5/8))**(1/4)
+
+print(dzialanieTrzecie())
