98316dd36f147c853a6813fb52550af3feee2311
replay20 <taglador@gmail.com>
2024-03-13 11:29:16 +0100

    Zad 1 poprawione z zaokragleniem

---
+++ lab4/main.py
@@ -0,0 +1,26 @@
+import math
+
+def dzialaniePierwsze():
+    return round((math.e**4 - math.log(34,2))**(1/3),2)
+
+print(dzialaniePierwsze())
+
+def dzialanieDrugie():
+    return round((math.log(20) + math.cos(45) + math.e)**(1/3),2)
+
+print(dzialanieDrugie())
+
+def dzialanieTrzecie():
+    return round((math.log(32,2)+math.sin(45) * (5/8))**(1/4),2)
+
+print(dzialanieTrzecie())
+
+def dzialanieCzwarte():
+    return round(math.log(23,3) + (math.sin(34) + 5) ** (1/3),2)
+
+print(dzialanieCzwarte())
+
+def dzialaniePiate():
+    return round((math.log(32,2) + math.pi + math.sin(56)) ** (1/4),2)
+
+print(dzialaniePiate())
