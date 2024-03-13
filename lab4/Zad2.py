53eb75311a686ce501757a47122ede724568bc24
replay20 <taglador@gmail.com>
2024-03-13 11:45:21 +0100

    Zad 2 gotowe

--- lab4/Zad2.py
+++ lab4/Zad2.py
@@ -4,10 +4,8 @@ def wiezaA(a):
 
 
 a = input("Podaj wysokosc wiezy (nie wiecej niz 10): ")
-a = int(a)
 
-
-if (a<=10) & (a>0):
-    wiezaA(a)
+if(a<=10 & a>0):
+    wiezaA(int(a))
 else:
     print("Podano zla wartosc")
